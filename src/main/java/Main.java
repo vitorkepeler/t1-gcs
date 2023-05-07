f13b6f711b26b6d563c97f10718b387f725e83d2
vkepeler <vitor.kepeler@e-core.com>
2023-05-07 16:29:09 -0300

    adding bee-1178

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,27 @@ import java.io.IOException;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Por favor, digite o número desejado: ");
+        double x;
+        x = sc.nextDouble();
+        bee1178(x);
+    }
 
+    public static void bee1178(double x){
+        double n[];
+        n = new double [100];
+        n[0] = x;
+
+        for(int i = 1; i < 100; i++){
+            x = x/2;
+            n[i] = x;
+        }
+
+        for(int i = 0; i < 100; i++){
+            System.out.print("N[" + i + "] = ");
+            System.out.format("%.4f", n[i]);
+            System.out.println();
+        }
     }
 }
